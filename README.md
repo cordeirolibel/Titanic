3202c68f623c2316595580d9ed6f4a0e446d8b5e
cordeirolibel <gustavolibel@gmail.com>
2017-10-12 19:54:29 -0300

    add pandas

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Reference: https://www.kaggle.com/c/titanic
 Machine Learning to predict which passengers survived the tragedy.
 
 Python 3: 
-numpy
+numpy e pandas
 
 ![alt text](https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/RMS_Titanic_4.jpg/1236px-RMS_Titanic_4.jpg)
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #========================================================
-# CordeiroLibel 2017 - https://github.com/cordeirolibel/
-# 
+# cordeirolibel and daviboberg - 2017
+# https://github.com/cordeirolibel/Titanic/
 #========================================================
 
-print('Hello World!')
+import pandas as pd
+import numpy as np
+
+#   Dataframes
+df_train = pd.read_csv('data/train.csv')
+df_test = pd.read_csv('data/test.csv')
+
+print(df_train.head())
